c72b4b248764a6076898ace42869426ac1432ba2
wrste13 <vebrovam@spsstav.cz>
2023-04-17 10:24:37 +0200

    commit 1704

--- projekt.js
+++ projekt.js
@@ -1,2 +1,22 @@
-document.write("")
+document.write("Objem a Obsah Koule")
 document.write("<br>")
+function objem(polomer){
+    return (4/3)*(22/7)*polomer*polomer*polomer
+}
+function obsah(polomer){
+        return (22/7)*4*polomer*polomer
+}
+function vypoctiObjem(){
+    let polomer= document.getElementById("polomer").value;
+    let objemm=objem(polomer)
+}
+function vypoctiObsah(){
+    let polomer= document.getElementById("polomer").value;
+    let obsahh=obsah(polomer)
+}
+document.getElementById("vysledek").innerText=vypoctiObjem();
+
+document.write("<br>");
+document.write("<br>");
+document.write("Objem: " + objemm + "cm");
+document.write("Obsah: " + obsahh + "cm");
