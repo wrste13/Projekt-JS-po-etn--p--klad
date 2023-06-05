8467cdcf3ffdfdb598ed77a5a4f85dbe44af32e0
wrste13 <vebrovam@spsstav.cz>
2023-06-05 10:27:33 +0200

    doplnění projektu

--- projekt.js
+++ projekt.js
@@ -1,5 +1,3 @@
-document.write("Objem a Obsah Koule")
-document.write("<br>")
 function objem(polomer){
     return (4/3)*(22/7)*polomer*polomer*polomer
 }
@@ -7,16 +5,36 @@ function obsah(polomer){
         return (22/7)*4*polomer*polomer
 }
 function vypoctiObjem(){
-    let polomer= document.getElementById("polomer").value;
-    let objemm=objem(polomer)
+    let polomer = document.getElementById("polomer").value;
+    let objemm = objem(polomer);
+    document.getElementById("vysledek1").innerText=objemm;
 }
+
 function vypoctiObsah(){
     let polomer= document.getElementById("polomer").value;
     let obsahh=obsah(polomer)
+    document.getElementById("vysledek2").innerText=obsahh;
 }
-document.getElementById("vysledek").innerText=vypoctiObjem();
 
-document.write("<br>");
-document.write("<br>");
-document.write("Objem: " + objemm + "cm");
-document.write("Obsah: " + obsahh + "cm");
+function objem2(polomer2){
+    return (4/3)*(22/7)*polomer2*polomer2*polomer2
+}
+function obsah2(polomer2){
+        return (22/7)*4*polomer2*polomer2
+}
+
+function zkontrolujObjem(){
+    if (objem2==obj){
+        document.getElementById("vysledek3").innerText="Správně";
+    } else {  
+            document.getElementById("vysledek3").innerText="Špatně";
+    } 
+}
+
+function zkontrolujObsah(){
+    if (obsah2==pvr){
+        document.getElementById("vysledek4").innerText="Správně";
+    } else {  
+            document.getElementById("vysledek4").innerText="Špatně";
+    } 
+}
